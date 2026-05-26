bca7df9a737d212d557442c600dd13f0c5c025b5
Pabrien <ren.mz.nexus@gmail.com>
2026-05-26 21:43:54 +0900

    Reduce UI copy and simplify controls

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const cacheName = "goalflow-v46-20260526-simpleintro";
+const cacheName = "goalflow-v47-20260526-iconui";
 const coreAssets = [
   "./",
   "./index.html",
-  "./styles.css?v=20260526-simpleintro",
-  "./app.js?v=20260526-simpleintro",
+  "./styles.css?v=20260526-iconui",
+  "./app.js?v=20260526-iconui",
   "./vendor/gsap.min.js",
   "./vendor/lottie.min.js",
   "./vendor/howler.min.js",
